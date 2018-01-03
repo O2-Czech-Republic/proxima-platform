219ebae874fdbf8fde83f4ae9e9412378f2a3eb0
Jan Lukavsky <je.ik@seznam.cz>
2018-01-03 11:40:17 +0100

    [proxima-gcloud-storage] #47 add gcloud module storage IO

---
+++ gcloud-storage/src/main/java/cz/o2/proxima/gcloud/storage/GCloudClient.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright 2017-2018 O2 Czech Republic, a.s.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cz.o2.proxima.gcloud.storage;
+
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageClass;
+import com.google.cloud.storage.StorageOptions;
+import cz.o2.proxima.repository.EntityDescriptor;
+import cz.o2.proxima.storage.AbstractStorage;
+import java.net.URI;
+import java.util.Map;
+import java.util.Optional;
+import lombok.Getter;
+
+class GCloudClient extends AbstractStorage {
+
+  @Getter
+  final Map<String, Object> cfg;
+
+  @Getter
+  final Storage client;
+
+  @Getter
+  final String bucket;
+
+  @Getter
+  final String path;
+
+  @Getter
+  final StorageClass storageClass;
+
+  GCloudClient(EntityDescriptor entityDesc, URI uri, Map<String, Object> cfg) {
+    super(entityDesc, uri);
+    this.cfg = cfg;
+    this.client = StorageOptions.getDefaultInstance().getService();
+    this.bucket = uri.getAuthority();
+    this.path = toPath(uri);
+    this.storageClass = Optional.ofNullable(cfg.get("storage-class"))
+        .map(Object::toString)
+        .map(StorageClass::valueOf)
+        .orElse(StorageClass.STANDARD);
+  }
+
+  // normalize path to not start and to end with slash
+  private static String toPath(URI uri) {
+    String p = uri.getPath();
+    while (p.startsWith("/")) {
+      p = p.substring(1);
+    }
+    while (p.endsWith("/")) {
+      p = p.substring(0, p.length() - 1);
+    }
+    return p + "/";
+  }
+
+  Blob createBlob(String name) {
+    return client.create(
+        BlobInfo.newBuilder(bucket, path + name)
+            .setStorageClass(storageClass)
+            .build(),
+        Storage.BlobTargetOption.doesNotExist());
+  }
+
+}
---
+++ gcloud-storage/src/test/java/cz/o2/proxima/gcloud/storage/BulkGCloudStorageWriterTest.java
@@ -0,0 +1,161 @@
+/**
+ * Copyright 2017-2018 O2 Czech Republic, a.s.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cz.o2.proxima.gcloud.storage;
+
+import com.google.cloud.storage.Blob;
+import com.typesafe.config.ConfigFactory;
+import cz.o2.proxima.repository.AttributeDescriptor;
+import cz.o2.proxima.repository.AttributeDescriptorBase;
+import cz.o2.proxima.repository.EntityDescriptor;
+import cz.o2.proxima.repository.Repository;
+import cz.o2.proxima.storage.StreamElement;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import static org.mockito.Mockito.*;
+
+/**
+ * Test suite for {@link BulkGCloudStorageWriter}.
+ */
+@RunWith(Parameterized.class)
+public class BulkGCloudStorageWriterTest {
+
+  final Repository repo = Repository.of(ConfigFactory.load().resolve());
+  final AttributeDescriptor<?> attr;
+  final AttributeDescriptor<?> wildcard;
+  final EntityDescriptor entity;
+
+  @Parameterized.Parameter
+  public boolean gzip;
+
+  @Parameterized.Parameters
+  public static Collection<Boolean> parameters() {
+    return Arrays.asList(true, false);
+  }
+
+  BulkGCloudStorageWriter writer;
+  List<String> blobs;
+  List<StreamElement> written;
+
+  public BulkGCloudStorageWriterTest() throws URISyntaxException {
+    this.wildcard = AttributeDescriptor.newBuilder(repo)
+        .setEntity("dummy")
+        .setSchemeURI(new URI("bytes:///"))
+        .setName("wildcard.*")
+        .build();
+    this.attr = AttributeDescriptor.newBuilder(repo)
+        .setEntity("dummy")
+        .setSchemeURI(new URI("bytes:///"))
+        .setName("attr")
+        .build();
+    this.entity = EntityDescriptor.newBuilder()
+        .setName("dummy")
+        .addAttribute((AttributeDescriptorBase<?>) attr)
+        .addAttribute((AttributeDescriptorBase<?>) wildcard)
+        .build();
+  }
+
+  @Before
+  public void setUp() throws URISyntaxException {
+    blobs = Collections.synchronizedList(new ArrayList<>());
+    written = null;
+    writer = new BulkGCloudStorageWriter(
+        entity, new URI("gcloud-storage://project:bucket/path"),
+        cfg()) {
+
+      @Override
+      Blob createBlob(String name) {
+        blobs.add(name);
+        return mock(Blob.class);
+      }
+
+      @Override
+      void flushToBlob(File file, Blob blob) throws IOException {
+        written = new ArrayList<>();
+        try (BinaryBlob.Reader reader = new BinaryBlob(file).reader(entity)) {
+          reader.iterator().forEachRemaining(written::add);
+        }
+      }
+
+    };
+  }
+
+  @Test(timeout = 2000)
+  public void testWrite() throws Exception {
+    CountDownLatch latch = new CountDownLatch(1);
+    long now = 1500000000000L;
+    StreamElement first = StreamElement.update(entity, attr,
+        UUID.randomUUID().toString(),
+        "key", "attr", now, new byte[] { 1, 2 });
+    StreamElement second = StreamElement.update(entity, wildcard,
+        UUID.randomUUID().toString(),
+        "key", "wildcard.1", now + 2000, new byte[] { 3 });
+    writer.write(first, (succ, exc) -> {
+      // this one will not get committed
+    });
+    TimeUnit.MILLISECONDS.sleep(1500);
+    writer.write(second, (succ, exc) -> {
+      assertTrue("Exception " + exc, succ);
+      assertNull(exc);
+      latch.countDown();
+    });
+    latch.await();
+    assertNotNull(written);
+    validate(written, first, second);
+    assertEquals(1, blobs.size());
+    assertEquals(
+        writer.toBlobName(now - now % 1000, now + 2000),
+        blobs.get(0));
+    assertTrue(blobs.get(0).startsWith("2017/07/"));
+  }
+
+  private void validate(List<StreamElement> written, StreamElement... elements)
+      throws IOException {
+
+    Iterator<StreamElement> iterator = written.iterator();
+    for (StreamElement el : elements) {
+      StreamElement next = iterator.next();
+      assertEquals(next.toString(), el.toString());
+    }
+  }
+
+  private Map<String, Object> cfg() {
+    Map<String, Object> ret = new HashMap<>();
+    ret.put("log-roll-interval", "1000");
+    if (gzip) {
+      ret.put("gzip", "true");
+    }
+    return ret;
+  }
+
+}

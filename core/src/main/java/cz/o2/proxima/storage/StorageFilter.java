44572d895d46363004550856fe14d1aa8cf69abe
Jan Lukavsky <je.ik@seznam.cz>
2018-05-23 15:35:30 +0200

    [proxima-core] add basic compound filters

--- core/src/main/java/cz/o2/proxima/storage/StorageFilter.java
+++ core/src/main/java/cz/o2/proxima/storage/StorageFilter.java
@@ -17,6 +17,8 @@ package cz.o2.proxima.storage;
 
 import cz.o2.proxima.annotations.Stable;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A filter that is applied to each input data ingest.
@@ -24,6 +26,45 @@ import java.io.Serializable;
 @Stable
 @FunctionalInterface
 public interface StorageFilter extends Serializable {
+
+  /** Filter consisting of several filters with applied function. */
+  public static abstract class CompoundFilter implements StorageFilter {
+
+    protected final List<StorageFilter> filters = new ArrayList<>();
+
+    CompoundFilter(List<StorageFilter> filters) {
+      this.filters.addAll(filters);
+    }
+
+  }
+
+  /** Filter performing logical OR of several filters. */
+  public static class OrFilter extends CompoundFilter {
+
+    protected OrFilter(List<StorageFilter> filters) {
+      super(filters);
+    }
+
+    @Override
+    public boolean apply(StreamElement ingest) {
+      return filters.stream().anyMatch(f -> f.apply(ingest));
+    }
+
+  }
+
+  /** Filter performing logical AND of several filters. */
+  public static class AndFilter extends CompoundFilter {
+
+    protected AndFilter(List<StorageFilter> filters) {
+      super(filters);
+    }
+
+    @Override
+    public boolean apply(StreamElement ingest) {
+      return filters.stream().allMatch(f -> f.apply(ingest));
+    }
+
+  }
 
   /**
    * When returns {@code false} the input element is not stored in the

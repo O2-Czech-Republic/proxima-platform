ec813bb5f200f133ba5dc9c4a914f761466484d9
Jan Lukavsky <je.ik@seznam.cz>
2020-01-29 11:31:22 +0100

    [proxima] automatically sync with o2 repo using github actions

---
+++ .github/workflows/sync-o2-master.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Copyright 2017-2020 O2 Czech Republic, a.s.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+set -e
+
+if echo $GITHUB_REPOSITORY | grep datadrivencz >/dev/null; then
+
+  echo ${O2_DEPLOY_KEY} > ~/.ssh/id_ecdsa
+
+  git remote add o2 git@github.com:O2-Czech-Republic/proxima-platform.git
+
+  git fetch --all
+  git checkout -B o2-master o2/master
+  git pull --ff-only origin master
+  git push o2-master
+
+fi

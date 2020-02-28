e2ed6d44e52585b038fbe702803d2b2f43253ada
Jan Lukavsky <je.ik@seznam.cz>
2020-02-28 11:00:39 +0100

    [proxima-core] #135 add JsonSerializer

--- core/src/main/java/cz/o2/proxima/scheme/StringUtf8Serializer.java
+++ core/src/main/java/cz/o2/proxima/scheme/StringUtf8Serializer.java
@@ -22,6 +22,37 @@ import java.util.Optional;
 /** A {@link ValueSerializer} for Strings. */
 public class StringUtf8Serializer implements ValueSerializerFactory {
 
+  static class StringValueSerializer implements ValueSerializer<String> {
+
+    @Override
+    public Optional<String> deserialize(byte[] input) {
+      return Optional.of(new String(input, StandardCharsets.UTF_8));
+    }
+
+    @Override
+    public byte[] serialize(String value) {
+      return value.getBytes(StandardCharsets.UTF_8);
+    }
+
+    @Override
+    public String getDefault() {
+      return "";
+    }
+
+    @Override
+    public String asJsonValue(String value) {
+      return "\"" + value + "\"";
+    }
+
+    @Override
+    public String fromJsonValue(String json) {
+      if (json.startsWith("\"") && json.endsWith("\"")) {
+        return json.substring(1, json.length() - 1);
+      }
+      throw new IllegalArgumentException(json + "is not json string");
+    }
+  }
+
   @Override
   public String getAcceptableScheme() {
     return "string";
@@ -30,36 +61,7 @@ public class StringUtf8Serializer implements ValueSerializerFactory {
   @SuppressWarnings("unchecked")
   @Override
   public <T> ValueSerializer<T> getValueSerializer(URI specifier) {
-    return (ValueSerializer)
-        new ValueSerializer<String>() {
-          @Override
-          public Optional<String> deserialize(byte[] input) {
-            return Optional.of(new String(input, StandardCharsets.UTF_8));
-          }
-
-          @Override
-          public byte[] serialize(String value) {
-            return value.getBytes(StandardCharsets.UTF_8);
-          }
-
-          @Override
-          public String getDefault() {
-            return "";
-          }
-
-          @Override
-          public String asJsonValue(String value) {
-            return "\"" + value + "\"";
-          }
-
-          @Override
-          public String fromJsonValue(String json) {
-            if (json.startsWith("\"") && json.endsWith("\"")) {
-              return json.substring(1, json.length() - 1);
-            }
-            throw new IllegalArgumentException(json + "is not json string");
-          }
-        };
+    return (ValueSerializer) new StringValueSerializer();
   }
 
   @Override

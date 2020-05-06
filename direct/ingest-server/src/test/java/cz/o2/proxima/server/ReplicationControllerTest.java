c60e06960a06bf93d6340c3dd30ddec766dfab0e
Jan Lukavsky <je.ik@seznam.cz>
2020-05-06 10:00:07 +0200

    [proxima-direct-ingest-server] add test for bulk writer to ReplicationController

--- direct/ingest-server/src/test/java/cz/o2/proxima/server/ReplicationControllerTest.java
+++ direct/ingest-server/src/test/java/cz/o2/proxima/server/ReplicationControllerTest.java
@@ -24,10 +24,13 @@ import cz.o2.proxima.direct.commitlog.LogObserver.OffsetCommitter;
 import cz.o2.proxima.direct.commitlog.LogObserver.OnNextContext;
 import cz.o2.proxima.direct.commitlog.ObserveHandle;
 import cz.o2.proxima.direct.commitlog.Offset;
+import cz.o2.proxima.direct.core.BulkAttributeWriter;
 import cz.o2.proxima.direct.core.CommitCallback;
 import cz.o2.proxima.direct.core.DirectDataOperator;
 import cz.o2.proxima.direct.core.OnlineAttributeWriter;
 import cz.o2.proxima.direct.core.Partition;
+import cz.o2.proxima.direct.storage.InMemStorage;
+import cz.o2.proxima.functional.UnaryPredicate;
 import cz.o2.proxima.repository.AttributeDescriptor;
 import cz.o2.proxima.repository.EntityDescriptor;
 import cz.o2.proxima.repository.Repository;
@@ -54,12 +57,13 @@ public class ReplicationControllerTest {
   DirectDataOperator direct;
   ReplicationController controller;
   CompletableFuture<Void> future;
-  CountDownLatch livenessLatch = new CountDownLatch(1);
+  CountDownLatch livenessLatch;
   long now;
 
   @Before
   public void setUp() {
     direct = repo.getOrCreateOperator(DirectDataOperator.class);
+    livenessLatch = new CountDownLatch(1);
     controller =
         new ReplicationController(repo) {
           @Override
@@ -93,7 +97,7 @@ public class ReplicationControllerTest {
                     () -> new IllegalArgumentException("Missing commit log reader for data")),
             Sets.newHashSet(data),
             new PassthroughFilter(),
-            fakeWriter(written));
+            fakeOnlineWriter(written));
     try (ObserveHandle handle = observer.start()) {
       writeEvent();
       assertEquals(1, written.size());
@@ -114,7 +118,7 @@ public class ReplicationControllerTest {
                     () -> new IllegalArgumentException("Missing commit log reader for data")),
             Sets.newHashSet(data),
             ingest -> false,
-            fakeWriter(written));
+            fakeOnlineWriter(written));
     try (ObserveHandle handle = observer.start()) {
       writeEvent();
       assertEquals(0, written.size());
@@ -136,10 +140,10 @@ public class ReplicationControllerTest {
                     () -> new IllegalArgumentException("Missing commit log reader for data")),
             Sets.newHashSet(status),
             new PassthroughFilter(),
-            fakeWriter(written));
+            fakeOnlineWriter(written));
     AtomicInteger commits = new AtomicInteger();
     observer.onNext(
-        getUpdate(gateway, armed),
+        getUpdate(gateway, armed, now),
         new OnNextContext() {
           @Override
           public OffsetCommitter committer() {
@@ -165,7 +169,31 @@ public class ReplicationControllerTest {
     assertEquals(0, written.size());
   }
 
-  private OnlineAttributeWriter fakeWriter(List<StreamElement> written) {
+  @Test
+  public void testBulkReplication() throws InterruptedException {
+    List<StreamElement> written = new ArrayList<>();
+    AbstractRetryableLogObserver observer =
+        controller.createBulkObserver(
+            "consumer",
+            direct
+                .getCommitLogReader(data)
+                .orElseThrow(
+                    () -> new IllegalArgumentException("Missing commit log reader for data")),
+            Sets.newHashSet(data),
+            new PassthroughFilter(),
+            fakeBulkWriter(
+                written, stamp -> stamp == now - InMemStorage.getBoundedOutOfOrderness() + 100));
+    try (ObserveHandle handle = observer.start()) {
+      for (int i = 0; i < 10; i++) {
+        writeEvent(now + 20 * i);
+      }
+      assertEquals(6, written.size());
+    }
+    livenessLatch.await();
+    assertEquals(1.0, Metrics.LIVENESS.getValue(), 0.0001);
+  }
+
+  private OnlineAttributeWriter fakeOnlineWriter(List<StreamElement> written) {
     return new OnlineAttributeWriter() {
       @Override
       public void write(StreamElement data, CommitCallback statusCallback) {
@@ -175,7 +203,39 @@ public class ReplicationControllerTest {
 
       @Override
       public URI getUri() {
-        return URI.create("fake:///");
+        return URI.create("fake-online:///");
+      }
+
+      @Override
+      public void close() {}
+    };
+  }
+
+  private BulkAttributeWriter fakeBulkWriter(
+      List<StreamElement> written, UnaryPredicate<Long> commitWatermarkPredicate) {
+
+    return new BulkAttributeWriter() {
+
+      private final List<StreamElement> buffered = new ArrayList<>();
+
+      @Override
+      public void write(StreamElement data, long watermark, CommitCallback statusCallback) {
+        buffered.add(data);
+        if (commitWatermarkPredicate.apply(watermark)) {
+          written.addAll(buffered);
+          buffered.clear();
+          statusCallback.commit(true, null);
+        }
+      }
+
+      @Override
+      public URI getUri() {
+        return URI.create("fake-bulk:///");
+      }
+
+      @Override
+      public void rollback() {
+        written.clear();
       }
 
       @Override
@@ -184,20 +244,26 @@ public class ReplicationControllerTest {
   }
 
   private void writeEvent() {
+    writeEvent(now);
+  }
+
+  private void writeEvent(long stamp) {
     direct
         .getWriter(data)
         .orElseThrow(() -> new IllegalArgumentException("Missing writer for data"))
-        .write(getUpdate(event, data), (succ, exc) -> {});
+        .write(getUpdate(event, data, stamp), (succ, exc) -> {});
   }
 
-  private StreamElement getUpdate(EntityDescriptor entity, AttributeDescriptor<?> attr) {
+  private static StreamElement getUpdate(
+      EntityDescriptor entity, AttributeDescriptor<?> attr, long stamp) {
+
     return StreamElement.upsert(
         entity,
         attr,
         UUID.randomUUID().toString(),
         UUID.randomUUID().toString(),
         attr.getName(),
-        now,
+        stamp,
         new byte[] {});
   }
 }

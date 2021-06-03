517f1c3b4d972470d570e0c255ab263969c33b78
David Moravek <dmvk@apache.org>
2021-06-03 14:38:05 +0200

    [flink-core] Sonar.

--- flink/core/src/test/java/cz/o2/proxima/flink/core/ResultExtractorTest.java
+++ flink/core/src/test/java/cz/o2/proxima/flink/core/ResultExtractorTest.java
@@ -27,7 +27,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class ResultExtractorTest {
+class ResultExtractorTest {
 
   @Test
   void testParsed(@Mock StreamElement element) {

76e765da550ad4940d27bb63e4ee9330aaae2a4d
Lukas Drbal <lukas.drbal@gmail.com>
2021-06-08 09:43:30 +0200

    Remove cache from proto schema extraction to avoid Recursive update
    Fix exceptions like
    ```
    java.lang.IllegalStateException: Recursive update
        at java.base/java.util.concurrent.ConcurrentHashMap.computeIfAbsent(ConcurrentHashMap.java:1763)
        at cz.o2.proxima.scheme.proto.utils.ProtoUtils.convertField(ProtoUtils.java:117)
        at cz.o2.proxima.scheme.proto.utils.ProtoUtils.lambda$convertProtoMessage$0(ProtoUtils.java:65)
    ```

--- scheme/proto/src/main/java/cz/o2/proxima/scheme/proto/utils/ProtoUtils.java
+++ scheme/proto/src/main/java/cz/o2/proxima/scheme/proto/utils/ProtoUtils.java
@@ -24,7 +24,6 @@ import cz.o2.proxima.scheme.SchemaDescriptors;
 import cz.o2.proxima.scheme.SchemaDescriptors.SchemaTypeDescriptor;
 import cz.o2.proxima.scheme.SchemaDescriptors.StructureTypeDescriptor;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 
@@ -44,11 +43,10 @@ public class ProtoUtils {
    */
   public static <T extends Message> StructureTypeDescriptor<T> convertProtoToSchema(
       Descriptor proto) {
-    return convertProtoMessage(proto, new ConcurrentHashMap<>());
+    return convertProtoMessage(proto);
   }
 
-  static <T extends Message> StructureTypeDescriptor<T> convertProtoMessage(
-      Descriptor proto, Map<String, SchemaTypeDescriptor<?>> structCache) {
+  static <T extends Message> StructureTypeDescriptor<T> convertProtoMessage(Descriptor proto) {
     final Map<String, SchemaTypeDescriptor<?>> fields =
         proto
             .getFields()
@@ -62,7 +60,7 @@ public class ProtoUtils {
                         throw new UnsupportedOperationException(
                             "Recursion in field [" + field.getName() + "] is not supported");
                       }
-                      return convertField(field, structCache);
+                      return convertField(field);
                     }));
 
     return SchemaDescriptors.structures(proto.getName(), fields);
@@ -76,8 +74,7 @@ public class ProtoUtils {
    * @return schema type descriptor
    */
   @SuppressWarnings("unchecked")
-  static <T> SchemaTypeDescriptor<T> convertField(
-      FieldDescriptor proto, Map<String, SchemaTypeDescriptor<?>> structCache) {
+  static <T> SchemaTypeDescriptor<T> convertField(FieldDescriptor proto) {
     SchemaTypeDescriptor<T> descriptor;
     switch (proto.getJavaType()) {
       case STRING:
@@ -113,10 +110,7 @@ public class ProtoUtils {
                         .collect(Collectors.toList()));
         break;
       case MESSAGE:
-        final String messageTypeName = proto.getMessageType().toProto().getName();
-        structCache.computeIfAbsent(
-            messageTypeName, name -> convertProtoMessage(proto.getMessageType(), structCache));
-        descriptor = (SchemaTypeDescriptor<T>) structCache.get(messageTypeName);
+        descriptor = (SchemaTypeDescriptor<T>) convertProtoMessage(proto.getMessageType());
 
         break;
       default:

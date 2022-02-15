a26bd82fd4376c4eee1327fb65385ce6389344d8
Jan Lukavsky <je.ik@seznam.cz>
2022-02-15 16:02:02 +0100

    [infra] update license plugin to 4.1

--- tools/src/main/groovy/cz/o2/proxima/tools/groovy/internal/ProximaInterpreter.groovy
+++ tools/src/main/groovy/cz/o2/proxima/tools/groovy/internal/ProximaInterpreter.groovy
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package cz.o2.proxima.tools.groovy.internal
 
 import java.lang.reflect.Method

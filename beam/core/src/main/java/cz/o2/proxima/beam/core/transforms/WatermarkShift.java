3a5509236dbc315a698d62e9cfc13ce7f863c7ea
Jan Lukavsky <je.ik@seznam.cz>
2024-12-13 14:15:16 +0100

    [proxima-beam-core] remove fork of CalendarWindows

--- beam/core/src/main/java/cz/o2/proxima/beam/core/transforms/WatermarkShift.java
+++ beam/core/src/main/java/cz/o2/proxima/beam/core/transforms/WatermarkShift.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package cz.o2.proxima.beam.util;
+package cz.o2.proxima.beam.core.transforms;
 
 import cz.o2.proxima.beam.util.state.ExcludeExternal;
 import org.apache.beam.sdk.state.TimeDomain;
